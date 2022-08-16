3fbc2d8406d3f1033ede68ebd5f8e5526bb3c93b
teddirussell <74998818+teddirussell@users.noreply.github.com>
2022-08-17 09:46:54 +1000

    Temperature Adjustment

--- src/script.js
+++ src/script.js
@@ -83,9 +83,19 @@ function formatDay(timestamp) {
   }
   function displayFahrenheitTemperature(event) {
     event.preventDefault();
-    let fahrenheitTemperature = (celsiusTemperature * 9/5) + 32;
+   celsiusLink.classList.remove("active");
+   fahrenheitLink.classList.add("active");
+   let fahrenheitTemperature = (celsiusTemperature * 9/5) + 32;
     let temperatureElement = document.querySelector("#temperature");
     temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+  }
+
+  function displayCelsiusTemperature(event) {
+event.preventDefault;
+celsiusLink.classList.add("active");
+fahrenheitLink.classList.remove("active");
+let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
   }
 
   let celsiusTemperature = null;
@@ -103,5 +113,8 @@ function formatDay(timestamp) {
  
   let fahrenheitLink= document.querySelector("#fahrenheit-link")
   fahrenheitLink.addEventListener("click", displayFahrenheitTemperature)
+
+  let celsiusLink= document.querySelector("#celsius-link")
+celsiusLink.addEventListener("click", displayCelsiusTemperature)
 
   searchCity("New York");
